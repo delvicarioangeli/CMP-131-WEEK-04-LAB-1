a34920da377d755f6897a469397a2ad9d59397c9
delvicario.angeli <delvicario.angeli@student.ccm.edu>
2026-09-16 09:49:03 -0400

    Completed Week 4 Lab 01 Assignment

--- interest_earned.py
+++ interest_earned.py
@@ -1,4 +1,17 @@
 # Name: Angelina Del Vicario
+# Lab: 01
+# Week: 04 
+# Date: 09/13/2026
 #
-#
-#
+principal = float(input("Enter the starting principal amount ($):"))
+annual_rate_percent = flloat(input("Enter the annual intrest rate (as a %):"))
+compounding_periods = int(input("Enter the number of times intrest is compounded in a year"))
+
+r = annual_rate_percent / 100
+
+amount_after_one_year = principal * (1 + r / compounding_periods) ** compounding_periods
+intrest_earned = amount_after_one_year - principal
+
+print ("Initial Deposit: $" + principal )
+print ("Intrest Earned after 1 year:  $" +intrest_earned)
+print ("Ending balance: $" + amount_after_one_year)
